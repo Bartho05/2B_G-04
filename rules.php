14d2e036c3ccc7c7b062a8f1edeb288cb922fb4d
Bartholomeo Rocha <eliasbartholomeu17@gmail.com>
2026-07-01 15:35:52 -0300

    Update rules.php

--- rules.php
+++ rules.php
@@ -1,1 +1,19 @@
 <?php
+
+// Verifica se o usuário está logado
+function isLoggedIn()
+{
+    return isset($_SESSION['user']);
+}
+
+// Retorna os dados do usuário logado (ou null)
+function getLoggedUser()
+{
+    return $_SESSION['user'] ?? null;
+}
+
+// Verifica se o usuário logado tem um tipo específico (admin, moderator, user)
+function hasType($type)
+{
+    return isLoggedIn() && $_SESSION['user']['type'] === $type;
+}
